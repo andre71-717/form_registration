84bef25217d1ea80c5542f3f921cac00dccf871c
andre71-717 <manch.simonyan787.ads@gmail.com>
2022-05-09 23:33:26 +0400

    js changes

--- index.js
+++ index.js
@@ -9,40 +9,46 @@ let registationForm =()=>{
 
     if(name.value.length < 2 || name.value.length > 10){
         alert("Name length should be more then 2 and less then 10")
-        name.focus()
+        name.style.borderColor = "red"
         return false
     }
 
-    // let mail_format = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
-
-    // if(!email.value.match(mail_format)){
-    //     alert("Please enter a valid email")
-    //     email.focus()
-    //     return false
-    // }
+    if(!email.value.includes("@gmail.com") && !email.value.includes("@mail.com")){
+        alert("Your mail must contain @mail.com or @gmail.com")
+        email.style.borderColor = "red"
+        return false
+    }
 
     if(password.value.length < 5 ){
         alert("Password length must be more then 5")
-        password.focus()
+        password.style.borderColor = "red"
         return false
     }
 
-    if(!phoneNumber.value[0] === 0 || !phoneNumber.value.length !== 9){
+    if(!phoneNumber.value[0] === 0 || phoneNumber.value.length !== 9){
         alert("Phone number length must be 9 characters long and first digit should be 0")
-        phoneNumber.focus()
+        phoneNumber.style.borderColor = "red"
         return false
     }
 
    if(zipcode.value.length < 5 || zipcode.value.length > 5){
        alert("Zip code length should be 5")
+       zipcode.style.borderColor = "red"
        return false
    }
 
    if(about.value === ""){
        alert("Writing about yourself is required")
+       about.style.borderColor = "red"
+       return false
    }
 
 
 return true
 
 }
+
+
+
+// let arman = "true";
+// console.log(arman == true && arman == false)
